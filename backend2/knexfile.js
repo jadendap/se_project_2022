2203b2c404e78a0bb7bd56b607e26e5d71a35800
mhinojo6 <77425565+mhinojo6@users.noreply.github.com>
2022-03-31 17:16:23 -0700

    Add files via upload

---
+++ backend2/knexfile.js
@@ -0,0 +1,16 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  client: "pg",
+  connection:
+    "", //database connection string needed
+  migrations: {
+    directory: "./src/migrations",
+  },
+  seeds: {
+    directory: "./src/seeds",
+  },
+};
